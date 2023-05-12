c4503d53cd73c2f3f3a64d8e944c2a83fa67e0b5
start.vaadin.com <no-reply@vaadin.com>
2023-05-12 07:51:19 +0000

    Generated project

---
+++ src/main/java/com/github/lotus/data/Role.java
@@ -0,0 +1,5 @@
+package com.github.lotus.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/github/lotus/data/service/SamplePersonRepository.java
@@ -0,0 +1,12 @@
+package com.github.lotus.data.service;
+
+import com.github.lotus.data.entity.SamplePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SamplePersonRepository
+        extends
+            JpaRepository<SamplePerson, Long>,
+            JpaSpecificationExecutor<SamplePerson> {
+
+}
